cbe5bd42b1128f1fe45bf0f2ea4add7b2581706e
no.mad <69174010+Tom-L-M@users.noreply.github.com>
2024-01-25 13:21:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const Client = require('./client');
+const Client = require('./src/client');
+module.exports = Client;
 
-(async function main () {
-    const c = new Client('127.0.0.1', 8007);
-    c.setDatabaseCredentials('root', 'root');
-    c.setNamespaceCredentials('main', 'user', 'user');
 
-    console.log( await c.listNamespaceUsers('main') );
-})();
+// Usage Example:
+// (async function main () {
+//     const c = new Client('localhost', 8000);
+//     c.setDatabaseCredentials('root', 'root');
+//     c.setNamespaceCredentials('main', 'user', 'user');
+//     console.log( await c.listNamespaceUsers('main') );
+// })();
